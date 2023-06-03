b3e59d1acc01f4dae12798fc846a6b9adb4b8d73
Haywhyskilashi <haywhyskilashi@gmail.com>
2023-06-03 18:12:34 +0100

    new

--- main.py
+++ main.py
@@ -81,5 +81,12 @@ else:
 
 ###############################################################
 
+words = ["water", "chair", "pen", "basket", "hi", "car"]
+num_words = 0
+for each in words:
+    if len(each) > 3:
+        num_words += 1
+
+print(num_words)
 
 
